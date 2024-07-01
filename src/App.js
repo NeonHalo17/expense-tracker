feeddd01addca94f599e169f7069378639462c7c
Aman <amanpj16@gmail.com>
2024-07-01 15:11:58 +0530

    changes test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-2xl">Hello World!</h1>
-  );
+    <div className="bg-slate-950">
+      <h1 className="text-center text-2xl text-emerald-500">Expense Tracker</h1>
+    </div>
+    );
 }
 
 export default App;
