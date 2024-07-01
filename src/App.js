bbcd22bae95f920bd910521d1b736dd9ae46b65d
Aman <amanpj16@gmail.com>
2024-07-01 15:12:55 +0530

    changes test2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-slate-950">
       <h1 className="text-center text-2xl text-emerald-500">Expense Tracker</h1>
-    </div>
+    </div>ssss
     );
 }
 
