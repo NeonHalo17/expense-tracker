28f4d83b9cdbc7ae3bb20109fcf2f5ce44d57035
Aman <amanpj16@gmail.com>
2024-07-02 17:25:07 +0530

    Navbar and add delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavbarTop from './components/NavbarTop';
 
 function App() {
   return (
-    <div className="bg-slate-950">
-      <h1 className="text-center text-2xl text-emerald-500">Expense Tracker</h1>
-    </div>ssss
+    <main>
+      <NavbarTop />
+    </main>
     );
 }
 
