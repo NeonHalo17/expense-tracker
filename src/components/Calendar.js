0ce61b32d9b0ac45aac5777ec2999978d82e3704
Aman <amanpj16@gmail.com>
2024-07-06 12:43:24 +0530

    some calendar changes

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,21 @@
+import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
+
 function Calendar() {
     return (
-        <h1 className="text-center mt-[300px] text-3xl"> Calendar Page!</h1>
+        <div className ="flex justify-center shrink">
+            <div className="mt-20 border shadow p-4">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar />
+                </LocalizationProvider>
+            </div>
+            <div className="mt-4 px-4 border mx-4">
+                <h1 className="mt-20 ml-20 pr-16">list of transactions goes here</h1>
+            </div>
+        </div>
     )
 }
 
